5148b4fe772857f244f6cca9285f9bc09a921143
knubbbe <knubbbe@gmail.com>
2018-12-11 11:58:35 +0100

    added sentry

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+import * as Sentry from '@sentry/browser';
 import App from './App.html';
 import store from './store.js';
+
+
+Sentry.init({ dsn: 'https://ffcd4a3639b14f82aae824e33fbd146b@sentry.io/1341813' });
 
 const app = new App({
 	target: document.body,
